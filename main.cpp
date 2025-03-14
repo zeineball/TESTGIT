3c45548bf8057eb5a690108f7cece60b2757b468
zeineb allani <zeineb.allani@esprit.tn>
2025-03-14 13:13:53 +0100

    Resolved merge conflicts

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+
+    return 0;
+}
